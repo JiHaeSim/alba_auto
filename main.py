76a373f1990ee876662014d6b19c2268091a2047
sim <simlucy0129@gmail.com>
2022-04-21 19:10:17 +0900

    scrap table test

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 import time
+from selenium.webdriver.support.ui import Select
+
 browser = webdriver.Chrome('chromedriver.exe')
 browser.get("https://www.albamon.com/login/logout_trans.asp")
 
@@ -20,9 +22,26 @@ browser.find_element_by_class_name('nav4').click()
 # browser.find_element_by_class_name('n2').click()
 # browser.switch_to.window(browser.window_handles[1])
 
+# 인재정보 50개씩 보기
+browser.find_element_by_class_name('selPerPage').click()
+select = Select(browser.find_element_by_class_name('selPerPage'))
+select.select_by_value('50')
+
+# 맞춤 인재정보 스크랩
+table = browser.find_element_by_class_name('tbl')
+rows = table.find_elements_by_tag_name("tr")
+# table 스크랩 테스트
+# for tr in table.find_elements_by_tag_name("tr"):
+#         td = tr.find_elements_by_tag_name("td")
+#         s = "{} , {}\n".format(td[1].text , td[2].text)
+#         #print (s)
+#         fp.write(s)
+
+# for index, value in enumerate(rows):
+    # body=value.find_elements_by_tag_name("td")[0]
+    # body = value.find_element_by_class_name('cTit')
+    # print(body.text)
+    # print(index.text,value.text)
 
 time.sleep(1000)
 
-
-
-# browser.find_element_by_xpath('//*[@id="power_link_body"]/ul/li[1]/div/a').get_attribute('href')
